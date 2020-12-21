3bcfc384775b1f29f62b088fd326130b96091dc3
allwaysoft <49237778+allwaysoft@users.noreply.github.com>
2020-12-21 19:16:22 +0800

    Add files via upload

--- MemoryEater.java
+++ MemoryEater.java
@@ -5,15 +5,20 @@ public class MemoryEater
   public static void main(String[] args)
   {
     Vector v = new Vector();
+    int i=0;
     while (true)
     {
       byte b[] = new byte[1048576];
       v.add(b);
+      i=i+1;
       Runtime rt = Runtime.getRuntime();
       System.out.println( "Total Memory = "
 + rt.totalMemory()
 + " Free Memory = "
-+ rt.freeMemory() );
++ rt.freeMemory()
++ " Allocated Memory = "
++ i
++ "MB" );
     }
   }
 }
